d10c1024caf5a402791fd4dd4440dd03b16b715b
Theviyanthan Krishnamohan <theviyanthan@outlook.com>
2020-11-20 00:36:04 +0530

    Fix typo

--- workers.d.ts
+++ workers.d.ts
@@ -1,4 +1,4 @@
 declare module 'web-worker:*' {
-    const WokerFactory: new () => Worker;
-    export default WokerFactory;
+    const WorkerFactory: new () => Worker;
+    export default WorkerFactory;
 }
